08c33a2f81c030cfa41cd5d3df893c09c5da5887
pabon980 <pabon980as@gmail.com>
2026-02-22 11:02:29 -0500

    clase objeto

---
+++ objlibro.java
@@ -0,0 +1,43 @@
+public class objlibro{
+    private String nombre;
+    private String autor;
+    private double precio;
+
+    public objlibro() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    
+
+    
+
+
+
+
+
+
+
+}
